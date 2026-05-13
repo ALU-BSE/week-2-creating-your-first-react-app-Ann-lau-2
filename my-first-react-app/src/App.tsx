e156ba1a5595f0a1106e83afb03871c6613ee32e
Ann Lau <101356391+Ann-lau@users.noreply.github.com>
2026-05-13 14:19:25 +0300

    add counter with increment and decrement

--- my-first-react-app/src/App.tsx
+++ my-first-react-app/src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import MyComponent from './MyComponent';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <MyComponent title='Hello from MyComponent!' />
+      <MyComponent title="Hello from MyComponent!" />
+      <div className="counter-container">
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <span>{count}</span>
+        <button onClick={() => setCount(count - 1)}>-</button>
+      </div>
     </div>
   );
 }
