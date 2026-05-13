dca19cc0d0f4076cfc5eb128f36bd8140f5680c5
Ann Lau <101356391+Ann-lau@users.noreply.github.com>
2026-05-13 13:39:17 +0300

    add CSS styling to MyComponent and App

--- my-first-react-app/src/MyComponent.tsx
+++ my-first-react-app/src/MyComponent.tsx
@@ -1,6 +1,8 @@
+import './MyComponent.css';
+
 const MyComponent = () => {
   return (
-    <div>
+    <div className='my-component'>
       <h1>Hello from MyComponent!</h1>
       <p>This is a paragraph of text within my component.</p>
     </div>
